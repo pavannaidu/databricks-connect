08feb6eab081d16e20cb65c4a15689f42983b801
Pavan Naidu <pavan.naidu@databricks.com>
2024-09-04 08:37:55 -0500

    init

---
+++ spaceflights-demo/run.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+%pip install -r "requirements.txt"
+
+# COMMAND ----------
+%load_ext kedro.ipython
+
+# COMMAND ----------
+%reload_kedro --env=databricks
+
+# COMMAND ----------
+session.run()
---
+++ spaceflights-demo/src/spaceflights_demo/__init__.py
@@ -0,0 +1,4 @@
+"""spaceflights-demo
+"""
+
+__version__ = "0.1"
